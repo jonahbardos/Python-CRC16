82af0c9988cd2bfcfebeaf864206a9f276889b83
Jonah Bardos <jonahbardos@gmail.com>
2021-07-05 00:43:54 -0700

    Added more comments

--- crc16_generator.py
+++ crc16_generator.py
@@ -1,7 +1,18 @@
 def crc16_generator(data):
-    '''
-    CRC-16-CCITT Algorithm
-    '''
+    """
+        CRC-16-CCITT Algorithm
+
+        Parameters
+        ----------
+        data : list
+
+        Returns
+        -------
+        list
+            crc flipped if 2 bytes
+            (e.g)
+            crc = 0xc0c1 -> [0xc1, 0xc0]
+    """
     data = bytearray(data)
     crc = 0xFFFF
     for b in data:
@@ -13,10 +24,39 @@ def crc16_generator(data):
 
             if bcarry:
                 crc ^= 0xa001
-    return crc
+    print("Generated CRC", (crc,), (hex(crc),))
 
+    if crc > 255:
+        hexstr = hex(crc)
+        x, y = int(hexstr[2:4], 16), int(hexstr[4:6], 16)
+        arr = [y, x]
+        print("Must send this list to receiever", arr,  hexstr[4:6], hexstr[2:4])
+        return arr
+
+    
+    arr = [crc]
+    print("Must send this list to receiever", arr)
+    return arr
+
+
+def check_crc(data, crc):
+    '''
+        For validation check:
+            - https://crccalc.com
+            - Algorithm - CRC-16/MODBUS
+    '''
+    data = bytearray(data)
+    for x in crc:
+        data.append(x)
+    
+    arr = crc16_generator(data)
+    if arr[0] == 0:
+        print("0 remainder", arr[0])
+        return
+    print("Invalid CRC")
+    print(arr)
+    
 
 if __name__ == "__main__":
-    x = crc16_generator([0xFF, 0xFE, 0xAC, 0x01, 0xed])
-    print(hex(x))
-    pass
+    x = crc16_generator([0xff, 0xfe])
+    check_crc([0xff, 0xfe], x)
