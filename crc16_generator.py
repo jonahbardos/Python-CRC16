6017b6e3343f4f009134a1c06a9285c78a022e19
Jonah <jonahbardos@gmail.com>
2022-11-12 17:08:28 -0600

    Added decimal and hex conversion

--- crc16_generator.py
+++ crc16_generator.py
@@ -1,27 +1,86 @@
-def crc16_generator(data):
-    """
-        CRC-16-CCITT Algorithm
+def crc16_generator_decimal(data: list[int]) -> int:
+    """CRC-16-MODBUS Algorithm
 
-        Parameters
-        ----------
-        data : list
+    Parameters
+    ----------
+    data : list[int]
+        Data packets received.
 
-        Returns
-        -------
-        list
-            crc flipped if 2 bytes
-            (e.g)
-            crc = 0xc0c1 -> [0xc1, 0xc0]
+    Returns
+    -------
+    int
+        CRC as an integer.
     """
     data = bytearray(data)
     crc = 0xFFFF
+    
+    # Calculate CRC-16 checksum for data packet
     for b in data:
         crc ^= b
         for _ in range(0, 8):
-            
             bcarry = crc & 0x0001
             crc >>= 1
+            if bcarry:
+                crc ^= 0xa001
+    
+    return crc
 
+
+def crc16_generator_hex(data: list[int]) -> str:
+    """CRC-16-MODBUS Algorithm
+
+    Parameters
+    ----------
+    data : list[int]
+        Data packets received.
+
+    Returns
+    -------
+    str
+        CRC as hex string
+    """
+    data = bytearray(data)
+    crc = 0xFFFF
+    
+    # Calculate CRC-16 checksum for data packet
+    for b in data:
+        crc ^= b
+        for _ in range(0, 8):
+            bcarry = crc & 0x0001
+            crc >>= 1
+            if bcarry:
+                crc ^= 0xa001
+    
+    return hex(crc)
+
+
+def crc16_generator(data: list[int]) -> list[int]:
+    """CRC-16-MODBUS Algorithm
+
+    Parameters
+    ----------
+    data : list
+        Data packets received.
+
+    Returns
+    -------
+    list
+        Returns CRC flipped
+    
+    Examples
+    --------
+    CRC calculated = 0xc0c1
+    Returns [0xc1, 0xc0]
+    """
+    data = bytearray(data)
+    crc = 0xFFFF
+    
+    # Calculate CRC-16 checksum for data packet
+    for b in data:
+        crc ^= b
+        for _ in range(0, 8):
+            bcarry = crc & 0x0001
+            crc >>= 1
             if bcarry:
                 crc ^= 0xa001
     print("Generated CRC", (crc,), (hex(crc),))
@@ -30,7 +89,7 @@ def crc16_generator(data):
         hexstr = hex(crc)
         x, y = int(hexstr[2:4], 16), int(hexstr[4:6], 16)
         arr = [y, x]
-        print("Must send this list to receiever", arr,  hexstr[4:6], hexstr[2:4])
+        # print("Must send this list to receiever", arr,  hexstr[4:6], hexstr[2:4])
         return arr
 
     
@@ -58,5 +117,10 @@ def check_crc(data, crc):
     
 
 if __name__ == "__main__":
-    x = crc16_generator([0xff, 0xfe])
-    check_crc([0xff, 0xfe], x)
+    data_packet = [0xff, 0xfe]
+    
+    # Driver Code Decimal
+    print(True if crc16_generator_decimal(data_packet) == 49345 else False)
+    
+    # x = crc16_generator([0xff, 0xfe])
+    # check_crc([0xff, 0xfe], x)
