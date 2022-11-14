e8db0e565bc5627ca0cbecbe1ff0860ebe688dcd
Jonah <jonahbardos@gmail.com>
2022-11-13 18:41:07 -0600

    Methods to verify Crc 16

--- crc16_generator.py
+++ crc16_generator.py
@@ -1,5 +1,5 @@
 def crc16_generator_decimal(data: list[int]) -> int:
-    """CRC-16-MODBUS Algorithm
+    """CRC-16-MODBUS Decimal Algorithm
 
     Parameters
     ----------
@@ -10,6 +10,11 @@ def crc16_generator_decimal(data: list[int]) -> int:
     -------
     int
         CRC as an integer.
+        
+    Raises
+    ----------
+    ValueError
+        If data packet in each index contains a byte > 256
     """
     data = bytearray(data)
     crc = 0xFFFF
@@ -27,7 +32,7 @@ def crc16_generator_decimal(data: list[int]) -> int:
 
 
 def crc16_generator_hex(data: list[int]) -> str:
-    """CRC-16-MODBUS Algorithm
+    """CRC-16-MODBUS Hex Algorithm
 
     Parameters
     ----------
@@ -38,6 +43,11 @@ def crc16_generator_hex(data: list[int]) -> str:
     -------
     str
         CRC as hex string
+    
+    Raises
+    ----------
+    ValueError
+        If data packet in each index contains a byte > 256
     """
     data = bytearray(data)
     crc = 0xFFFF
@@ -54,24 +64,27 @@ def crc16_generator_hex(data: list[int]) -> str:
     return hex(crc)
 
 
-def crc16_generator(data: list[int]) -> list[int]:
-    """CRC-16-MODBUS Algorithm
+def crc16_generator_to_send(data: list[int], debug=False) -> list[int]:
+    """CRC-16-MODBUS Algorithm to send to receiver.
 
     Parameters
     ----------
-    data : list
-        Data packets received.
+    data : list[int]
+        Data packet to create a checksum.
+    debug : bool, optional
+        Prints out generated CRC, by default False
 
     Returns
     -------
-    list
-        Returns CRC flipped
+    list[int]
+        Returns generated CRC into hex format flipped.
     
-    Examples
-    --------
-    CRC calculated = 0xc0c1
-    Returns [0xc1, 0xc0]
+    Raises
+    ----------
+    ValueError
+        If data packet in each index contains a byte > 256
     """
+    crc_to_send = list()
     data = bytearray(data)
     crc = 0xFFFF
     
@@ -83,44 +96,56 @@ def crc16_generator(data: list[int]) -> list[int]:
             crc >>= 1
             if bcarry:
                 crc ^= 0xa001
-    print("Generated CRC", (crc,), (hex(crc),))
-
-    if crc > 255:
-        hexstr = hex(crc)
-        x, y = int(hexstr[2:4], 16), int(hexstr[4:6], 16)
-        arr = [y, x]
-        # print("Must send this list to receiever", arr,  hexstr[4:6], hexstr[2:4])
-        return arr
-
     
-    arr = [crc]
-    print("Must send this list to receiever", arr)
-    return arr
+    if crc > 0xff:
+        msb = crc >> 0x08 & 0xff
+        lsb = crc & 0xff
+        crc_to_send = [lsb, msb]
+    else:
+        crc_to_send = [crc]
+
+    if debug:
+        print(f'Generated CRC: {crc}')
+        print(f'Converted CRC to hex: {hex(crc)}')
+        print(f'CRC to send to receiver: {crc_to_send}')
+
+    return crc_to_send
 
 
-def check_crc(data, crc):
-    '''
-        For validation check:
-            - https://crccalc.com
-            - Algorithm - CRC-16/MODBUS
-    '''
-    data = bytearray(data)
-    for x in crc:
-        data.append(x)
-    
-    arr = crc16_generator(data)
-    if arr[0] == 0:
-        print("0 remainder", arr[0])
-        return
-    print("Invalid CRC")
-    print(arr)
+def crc16_verify(data_packet: list[int], debug=False) -> bool:
+    """https://crccalc.com
+       Algorithm - CRC-16/MODBUS
+
+    Parameters
+    ----------
+    data_packet : list[int]
+        Data packet with CRC to verify.
+
+    Returns
+    -------
+    bool
+        True if CRC checksum no errors found, else False
+    """
+    data = bytearray(data_packet)
+    remainder = crc16_generator_to_send(data)
+    if debug:
+        print(f'Data packet: {data_packet} -> Remainder: {remainder}')
+    return (True if sum(remainder) == 0 else False)
     
 
 if __name__ == "__main__":
     data_packet = [0xff, 0xfe]
+    bad_data_packet = [0xff]
     
     # Driver Code Decimal
     print(True if crc16_generator_decimal(data_packet) == 49345 else False)
     
-    # x = crc16_generator([0xff, 0xfe])
-    # check_crc([0xff, 0xfe], x)
+    # Drive code Hex
+    print(True if crc16_generator_hex(data_packet) == '0xc0c1' else False)
+    
+    # Verify CRC is correct
+    data_packet.extend(crc16_generator_to_send(data_packet))
+    print(True if crc16_verify(data_packet) == True else False)
+    
+    bad_data_packet.extend(crc16_generator_to_send(bad_data_packet) + [0xff])
+    print(True if crc16_verify(bad_data_packet, debug=True) == True else False)
